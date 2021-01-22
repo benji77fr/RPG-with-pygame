661464e793349ca6370594a70cf47bd329876de1
Benjamin G <30464317+benji77fr@users.noreply.github.com>
2021-01-22 06:36:01 +0100

    Add files via upload

---
+++ sprites.py
@@ -0,0 +1,43 @@
+import pygame as pg
+import os
+from settings import *
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        # self.image = pg.Surface((24, 35))
+        self.image = pg.image.load(
+            os.path.join('Python/Assets', 'Player.png')
+        )
+        self.rect = self.image.get_rect()
+        self.vx, self.vy = 0, 0
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_q]:
+            self.vx = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_z]:
+            self.vy = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYER_SPEED
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071  # Equivalent à Sqrt(2)
+            self.vy *= 0.7071
+
+    def move(self, dx=0, dy=0):
+        self.x += dx
+        self.y += dy
+
+    def update(self):
+        self.get_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.topleft = (self.x, self.y)
